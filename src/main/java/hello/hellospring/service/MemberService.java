a343b67473ee94c8c0e9207902502af0df7d67c8
gltdhd <gltdhd@gmail.com>
2023-10-14 16:04:46 +0900

    23/10/14 회원 관리 예제 - 백엔드 개발

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,42 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member){
+        //같은 이름이 있는 중복 제외
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다");
+            });
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 전체 회원 조회
+     *
+     */
+    public List<Member> findMembers(){
+         return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberiD){
+        return memberRepository.findById(memberiD);
+    }
+}
